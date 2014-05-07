3b8311b715ca55efc1616251137addc7fc7f1e14
Mitsushige Komiya <s.wyvern.acespring@gmail.com>
2014-05-07 20:07:46 +0900

    Add player move

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -19,10 +19,30 @@ bool HelloWorld::init()
     
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Point origin = Director::getInstance()->getVisibleOrigin();
-
-    auto sprite = Sprite::create("player.png");
-    sprite->setPosition(Point(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-    this->addChild(sprite, 0);
+    
+    // プレイヤー表示
+    _player = Sprite::create("player.png");
+    _player->setPosition(Point(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+    this->addChild(_player, 0);
+    
+    // 操作系設定
+    auto dispatcher = Director::getInstance()->getEventDispatcher();
+    auto listener = EventListenerTouchOneByOne::create();
+    listener->onTouchBegan = [=](Touch* touch, Event* event) {
+        _prev_pos = touch->getLocation();
+        return true;
+    };
+    listener->onTouchMoved = [=](Touch* touch, Event* event) {
+        auto pos = touch->getLocation();
+        auto add_pos = pos - _prev_pos;
+        auto new_pos = _player->getPosition() + add_pos;
+        _player->setPosition(new_pos);
+        _prev_pos = pos;
+    };
+    listener->onTouchEnded = [=](Touch* touch, Event* event) {
+        _prev_pos = touch->getLocation();
+    };
+    dispatcher->addEventListenerWithFixedPriority(listener, 128);
     
     return true;
 }
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -9,6 +9,10 @@ public:
     static cocos2d::Scene* createScene();
     virtual bool init();
     CREATE_FUNC(HelloWorld);
+    
+private:
+    cocos2d::Sprite* _player;
+    cocos2d::Point _prev_pos;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
